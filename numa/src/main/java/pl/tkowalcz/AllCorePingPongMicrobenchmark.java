e527b0b3f5fb7d959ca2e2f38f0d15cc2b1e7abb
Tomasz Kowalczewski <tomasz.kowalczewski@codewise.com>
2018-11-20 23:40:48 +0100

    Removed incorrect copyright and migrated to using UNSAFE.

--- numa/src/main/java/pl/tkowalcz/AllCorePingPongMicrobenchmark.java
+++ numa/src/main/java/pl/tkowalcz/AllCorePingPongMicrobenchmark.java
@@ -1,35 +1,7 @@
-/*
- * Copyright (c) 2014, Oracle America, Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- *  * Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- *  * Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- *  * Neither the name of Oracle nor the names of its contributors may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
- * THE POSSIBILITY OF SUCH DAMAGE.
- */
 package pl.tkowalcz;
 
+import org.agrona.BufferUtil;
+import org.agrona.UnsafeAccess;
 import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.infra.Control;
 import org.openjdk.jmh.results.format.ResultFormatType;
@@ -37,12 +9,10 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import sun.misc.Unsafe;
 import xerial.jnuma.Numa;
 
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.VarHandle;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -50,10 +20,11 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
-@State(Scope.Group)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class AllCorePingPongMicrobenchmark {
+
+    private static final Unsafe UNSAFE = UnsafeAccess.UNSAFE;
 
     @State(Scope.Benchmark)
     public static class CoreSequence {
@@ -76,13 +47,12 @@ public class AllCorePingPongMicrobenchmark {
     @State(Scope.Group)
     public static class GroupState {
 
-        public final VarHandle handle = MethodHandles.byteBufferViewVarHandle(int[].class, ByteOrder.nativeOrder());
-
-        public ByteBuffer byteBuffer;
+        private long flagAddress;
 
         @Setup
         public void setUp(CoreSequence coreSequence) {
-            byteBuffer = Numa.allocOnNode(1024, coreSequence.core2CoreDescriptor.getCore1());
+            ByteBuffer byteBuffer = Numa.allocOnNode(1024, coreSequence.core2CoreDescriptor.getCore1());
+            flagAddress = BufferUtil.address(byteBuffer);
         }
     }
 
@@ -101,7 +71,7 @@ public class AllCorePingPongMicrobenchmark {
     @Benchmark
     @Group("pingpong")
     public void ping(Control cnt, GroupState groupState, CoreAssigner coreAssigner) {
-        while (!cnt.stopMeasurement && !groupState.handle.compareAndSet(groupState.byteBuffer, 0, 0, 1)) {
+        while (!cnt.stopMeasurement && !UNSAFE.compareAndSwapInt(null, groupState.flagAddress, 0, 1)) {
             // this body is intentionally left blank
         }
     }
@@ -109,7 +79,7 @@ public class AllCorePingPongMicrobenchmark {
     @Benchmark
     @Group("pingpong")
     public void pong(Control cnt, GroupState groupState, CoreAssigner coreAssigner) {
-        while (!cnt.stopMeasurement && !groupState.handle.compareAndSet(groupState.byteBuffer, 0, 1, 0)) {
+        while (!cnt.stopMeasurement && !UNSAFE.compareAndSwapInt(null, groupState.flagAddress, 1, 0)) {
             // this body is intentionally left blank
         }
     }
@@ -129,11 +99,12 @@ public class AllCorePingPongMicrobenchmark {
                 .warmupIterations(1)
                 .measurementIterations(1)
                 .param("coresConfigurations", coresConfigurations)
-//                .jvmArgs(
-//                        "-XX:+UnlockExperimentalVMOptions",
-//                        "-XX:+UseEpsilonGC"
-//                )
+                .jvmArgs(
+                        "-XX:+UnlockExperimentalVMOptions",
+                        "-XX:+UseEpsilonGC"
+                )
                 .resultFormat(ResultFormatType.CSV)
+//                .addProfiler(LinuxPerfAsmProfiler.class)
                 .threads(2)
                 .forks(1)
                 .build();
